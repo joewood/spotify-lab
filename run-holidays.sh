485deaa7b8dde4d497a624b384c0f0590120d967
Joe Wood <mail@joewood.me>
2024-12-18 13:21:18 -0500

    Orphans

--- run-holidays.sh
+++ run-holidays.sh
@@ -1,4 +1,7 @@
-python auto-list.py update-cache --playlist "Noise"  --playlist "Holiday Songs" --playlist "December Songs" --playlist "Variations on a Christmas Theme"
+python auto-list.py update-cache --playlist "Noise"  --playlist "Holiday Songs" \
+      --playlist "December Songs" --playlist "Variations on a Christmas Theme" \
+      --playlist "Christmas Blanket" --playlist "Gen-X Christmas" --playlist "Christmas Classics" \
+      --playlist "December Chill"
 
 python auto-list.py playlist "December" --include "December Songs"  --include "Holiday Songs" --include-holidays --update-cache
 python auto-list.py playlist "December Classical" --include "December Songs"  --include "Holiday Songs" \
@@ -8,8 +11,17 @@ python auto-list.py playlist "December Classical" --include "December Songs"  --
     --genre "string quartet" --genre "russian romanticism" \
     --genre "ukrainian classical" \
     --genre "classical guitar"  --include-holidays --update-cache
-python auto-list.py playlist "December This Year" --include "December Songs" --added-after=T-370 --include "Holiday Songs" --include "December Songs" --include-holidays --exclude-noise 
-python auto-list.py playlist "December New" --include "December Songs" --released-after=T-900  --include "Holiday Songs"  --include-holidays --exclude-noise 
-python auto-list.py playlist "December Classics" --include "December Songs" --released-before="1990-01-01" --include "Holiday Songs" --exclude "December Classical (A)" --include-holidays
-python auto-list.py playlist "December Pop" --include "December Songs"  --include "Holiday Songs" --exclude "December Classical (A)" --exclude "Variations on a Christmas Theme"  --include-holidays
-python auto-list.py playlist "December New Pop" --released-after=T-740  --include "December Songs"  --include "Holiday Songs" --exclude "December Classical (A)" --exclude "Variations on a Christmas Theme"  --include-holidays --exclude-noise 
+python auto-list.py playlist "December This Year" --include "December Songs" --added-after=T-370 \
+        --include "Holiday Songs" --include "December Songs" --include-holidays --exclude-noise 
+python auto-list.py playlist "December New" --include "December Songs" --released-after=T-900  \
+        --include "Holiday Songs"  --include-holidays --exclude-noise 
+python auto-list.py playlist "December Classics" --include "December Songs" --released-before="1990-01-01" \
+        --include "Holiday Songs" --exclude "December Classical (A)" --include-holidays
+python auto-list.py playlist "December Pop" --include "December Songs"  --include "Holiday Songs" \
+        --exclude "December Classical (A)" --exclude "Variations on a Christmas Theme"  --include-holidays
+python auto-list.py playlist "December New Pop" --released-after=T-740  --include "December Songs"  \
+        --include "Holiday Songs" --exclude "December Classical (A)" --exclude "Variations on a Christmas Theme"  \
+        --include-holidays --exclude-noise 
+python auto-list.py playlist "Christmas Orphans" --include "December Songs"  --include "Holiday Songs" \
+        --exclude "Christmas Blanket" --exclude "Gen-X Christmas"  --exclude "Variations on a Christmas Theme" \
+        --exclude "Modern Indie Christmas" --exclude "Christmas Classics" --exclude "December Chill" --include-holidays
